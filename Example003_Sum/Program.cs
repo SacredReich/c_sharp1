d713fd15219cf68af1057aa22bc9a5e561b06541
«DonRumata» <sinjit89231515468@gmail.com>
2023-03-13 19:39:58 +0600

    Sum RANDOM

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int nubmerB = new Random().Next(1, 10);
+Console.WriteLine(nubmerB);
+int result = numberA + nubmerB;
+Console.WriteLine(result);
