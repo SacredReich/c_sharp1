3c2d0fd7d38352ef85be0cf92830e3f5de9ea956
«DonRumata» <sinjit89231515468@gmail.com>
2023-03-15 09:48:20 +0600

    fail masha

--- Example005_IfElse/Program.cs
+++ Example005_IfElse/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.Write("Введите имя пользоватиеля ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша");
+{
+  Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+  Console.Write("Привет, ");
+  Console.Write(username);
+}
